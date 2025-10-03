2098e7e8986d9ea38d9255bb89544be0bd50678d
Firedemon575 <pablojoseporras23@gmail.com>
2025-10-03 07:23:52 +0200

    otra version

---
+++ script25.ps1
@@ -0,0 +1,17 @@
+﻿###############################################
+###############################################
+###############################################
+#############################################
+################ Lectura de Fichero#######
+
+$p = Get-Process
+$rutaFichero="procesos.xml"
+$l = $p.Length
+$cadena = "<procesos>"  
+
+for ($i = 0; $i -lt $l; $i++) {
+    $cadena = $cadena + "<procesos><name>" + $p[$i].ProcessName + "</name><id>" + $p[$i].Id + "</id></procesos>"
+ 
+}
+$cadena=$cadena + "</procesos>" 
+Set-Content -Path $rutaFichero -Value $cadena
