0cf9bcdd0a4011c92d3270a5dbeb2db7f83781b7
Firedemon575 <pablojoseporras23@gmail.com>
2025-10-03 10:02:42 +0200

    modificacion script

---
+++ script26.ps1
@@ -0,0 +1,18 @@
+﻿$p = Get-Localuser
+$rutafichero = "C:\xampp\htdocs\power\index2.html"
+$l = $p.length
+$cadena = "<html><head><style>
+body { font-family: Arial; background-color: #f4f4f4; }
+table { border-collapse: collapse; width: 60%; margin-top: 50px; }
+</style></head><body><center><table>
+<tr><th>Nombre</th><th>Descripción</th></tr>"
+
+for ($i = 0; $i -lt $l; $i++) {
+    $cadena = $cadena + "<tr><td>" + $p[$i].Name + "</td><td>" + $p[$i].Description + "</td></tr>"
+}
+
+$cadena = $cadena + "</table></center></body></html>"
+
+Write-Host $cadena
+Set-Content -Path $rutafichero -Value $cadena
+
